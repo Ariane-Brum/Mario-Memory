e71fc75a78dc3894e1e48d5b013a0a4d24dd1b6b
Ariane-Brum <arianebrum27@gmail.com>
2022-05-09 15:32:21 -0300

    :sparkles: Create Data

---
+++ src/data/items.js
@@ -0,0 +1,15 @@
+import mushroomBlue from "../assets/mushroom-blue.webp";
+import mushroomGreen from "../assets/mushroom-green.webp";
+import mushroom from "../assets/mushroom.webp";
+import racoonMario from "../assets/racoon-mario.webp";
+import flower from "../assets/flower.webp";
+import paperMario from "../assets/paper-mario.webp";
+
+export const items = [
+  { name: "mushroomBlue", icon: mushroomBlue },
+  { name: "mushroomGreen", icon: mushroomGreen },
+  { name: "mushroom", icon: mushroom },
+  { name: "racoonMario", icon: racoonMario },
+  { name: "flower", icon: flower },
+  { name: "paperMario", icon: paperMario },
+];
